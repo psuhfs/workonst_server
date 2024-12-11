1f8d31aba682b87c1ddd12438fa77a7197614faf
Aman Sahu <144287512+as9219@users.noreply.github.com>
2024-12-11 11:15:36 -0500

    feat: added generateEmailBody for stockon

--- src/handler/utils.ts
+++ src/handler/utils.ts
@@ -2,6 +2,7 @@
 // requests and responses.
 // This file should not contain any core logic.
 
+// WorkOn Interfaces
 export interface PointsDetails {
     accessCode: string;
     employeeName: string;
@@ -63,4 +64,14 @@ export function deSerializePointsDetails(details: PointsDetails) {
 
     // Serialize the object to a JSON string
     return JSON.stringify(modifiedDetails);
+}
+
+
+// StockOn Interfaces
+export interface OrderDetails {
+    email: string;
+    accessCode: string;
+    location: string;
+    deliveryDate: string;
+    items?: {};
 }
--- src/workspace/stockon/postHandlers.ts
+++ src/workspace/stockon/postHandlers.ts
@@ -1,5 +1,6 @@
 import {internalServerError, success} from "../../http/responseTemplates.ts";
 import type {CustomResponse} from "../../http/response.ts";
+import {type OrderDetails} from "../../handler/utils.ts";
 
 export async function handleSendEmail(_: Request): Promise<CustomResponse> {
     try {
@@ -8,3 +9,40 @@ export async function handleSendEmail(_: Request): Promise<CustomResponse> {
         return internalServerError(`An error occurred while trying to send email: ${e}`);
     }
 }
+
+function generateEmailBody(data: OrderDetails): string {
+    let message = `Hello, ${data.accessCode},\n\n`;
+    message += "Please find the attached order data file and a summary table below:\n";
+    message += `Location: ${data.location}\n`;
+    message += `Delivery Date: ${data.deliveryDate}\n`;
+    message += "Thank you,\nStudent Scheduler\n";
+
+    let tableRows = "";
+    for (const item of (Array.isArray(data.items) ? data.items : [])) {
+        tableRows += `<tr>
+                        <td>${item.Item_ID}</td>
+                        <td>${item.Name}</td>
+                        <td>${item.Unit_Size}</td>
+                        <td>${item.Order_Quantity}</td>
+                      </tr>`;
+    }
+    const orderTable = `<table border='1'>
+                          <thead>
+                              <tr>
+                                  <th>Item ID</th>
+                                  <th>Name</th>
+                                  <th>Unit Size</th>
+                                  <th>Order Quantity</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              ${tableRows}
+                          </tbody>
+                        </table>`;
+
+    message += orderTable;
+    message += "\nThank you, \nStudent Scheduler\n";
+
+    return message;
+}
+
