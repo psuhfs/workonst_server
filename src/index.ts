d289fe3d20ab5c74abc30d24db6c41d7931f1590
Sandipsinh Rathod <zotbysandip@gmail.com>
2025-01-31 11:15:17 -0500

    feat: modify OPTIONs handling

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ const server = Bun.serve({
           "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
           "Access-Control-Allow-Headers": "Content-Type, Authorization",
           "Access-Control-Max-Age": "86400", // Cache preflight response for 1 day
+          "Access-Control-Allow-Credentials": "true",
         },
       });
     }
