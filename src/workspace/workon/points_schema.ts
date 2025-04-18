8ce9c11789d814715c1a545cb8fdfc5e6603b454
Sandipsinh Rathod <zotbysandip@gmail.com>
2025-04-18 13:50:42 -0400

    switch workon to mongodb

---
+++ src/workspace/workon/points_schema.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import {Collections, DBCollection} from "../../handler/utils.ts";
+
+const uri = process.env.MONGO_URI;
+await mongoose.connect(uri ? uri : "", {
+    dbName: DBCollection.STOCKON
+});
+
+const pointsSchema = new mongoose.Schema({
+    accessCode: {type: String, required: true},
+    employeeName: {type: String, required: true},
+    employeeId: {type: String, required: true},
+    shiftDate: {type: Date, required: false},
+    selectedShift: {type: String, required: false},
+    manualShift: {type: String, required: false},
+    reason: {type: String, required: true},
+    comments: {type: String, required: false},
+    email: {type: String, required: true},
+    points: {type: Number, required: true},
+});
+
+
+export const POINTS_SCHEMA = mongoose.model(Collections.POINTS, pointsSchema);
--- src/workspace/workon/postHandlers.ts
+++ src/workspace/workon/postHandlers.ts
@@ -1,15 +1,16 @@
+import mongoose from "mongoose";
 import type {CustomResponse} from "../../http/response.ts";
-import {Db, prisma} from "../../handler/db.ts";
-import {type PointsDetails, Zone} from "../../handler/utils.ts";
+import {DBCollection, type PointsDetails, Zone} from "../../handler/utils.ts";
 import {Email} from "../../handler/email.ts";
 import {internalServerError, success} from "../../http/responseTemplates.ts";
 import {getShift} from "./employeeRecords.ts";
-import type {New_Table_Name} from "@prisma/client";
 import {handleAuth} from "../../auth/handler.ts";
 import type {RequestHandler} from "../../http/traits.ts";
 import {extractTokenDetails, extractTokenFromHeaders} from "../../auth/token_extractor.ts";
+import {POINTS_SCHEMA} from "./points_schema.ts";
 
-function pointDetToTable(body: PointsDetails): New_Table_Name | Error {
+
+function pointDetToTable(body: PointsDetails): object | Error {
     try {
         return {
             accessCode: body.accessCode,
@@ -72,7 +73,21 @@ export class IncrHandler implements RequestHandler {
                 body.accessCode = extractTokenDetails({token})["username"];
             }
 
-            new Db(prisma.new_Table_Name, body).send().then(populateErr);
+            const incr = new POINTS_SCHEMA({
+                accessCode: body.accessCode,
+                employeeName: body.employeeName,
+                employeeId: body.employeeId,
+                shiftDate: body.shiftDate,
+                selectedShift: body.selectedShift,
+                manualShift: body.manualShift,
+                reason: body.reason,
+                comments: body.comments,
+                email: body.email,
+                points: body.points,
+            })
+
+            await incr.save().then(populateErr);
+
             let email = {
                 subject: emailSubject,
                 to: body.email,
@@ -87,11 +102,6 @@ export class IncrHandler implements RequestHandler {
             }
 
             if (errors.length == 0) {
-                let pointD = await prisma.new_Table_Name.create({
-                    data: data,
-                });
-
-                console.debug(pointD);
                 return success(
                     {
                         success: "Email sent and Points inserted in db.",
