5a849d818b8efaf65ebf88d1f41c839201d3b508
Sandipsinh Rathod <zotbysandip@gmail.com>
2025-09-23 17:42:34 -0400

    feat: Enhance CSV generation by including header in output

--- src/workspace/stockon/generateOrderFile.ts
+++ src/workspace/stockon/generateOrderFile.ts
@@ -1,26 +1,32 @@
-import { createObjectCsvStringifier } from "csv-writer";
-import { type OrderDetails } from "../../handler/utils.ts";
+import {createObjectCsvStringifier} from "csv-writer";
+import {type OrderDetails} from "../../handler/utils.ts";
 
-export function generateCsvFromItems(orderDetails: [OrderDetails]): string {
-  try {
-    if (orderDetails.length === 0) {
-      console.error("No items to write to CSV.");
-      return "";
+export function generateCsvFromItems(
+    orderDetails: OrderDetails[]
+): string {
+    try {
+        if (orderDetails.length === 0) {
+            console.error("No items to write to CSV.");
+            return "";
+        }
+
+        const csvWriter = createObjectCsvStringifier({
+            header: [
+                {id: "item_id", title: "Item ID"},
+                {id: "name", title: "Name"},
+                {id: "unit_sz", title: "Unit Size"},
+                {id: "quantity", title: "Order Quantity"},
+                {id: "location", title: "Location"},
+                {id: "area", title: "Area"},
+                {id: "category", title: "Category"},
+            ],
+        });
+
+        const header = csvWriter.getHeaderString();
+        const records = csvWriter.stringifyRecords(orderDetails);
+
+        return header + records;
+    } catch (error) {
+        return "";
     }
-
-    const csvWriter = createObjectCsvStringifier({
-      header: [
-        { id: "item_id", title: "Item ID" },
-        { id: "name", title: "Name" },
-        { id: "unit_sz", title: "Unit Size" },
-        { id: "quantity", title: "Order Quantity" },
-        { id: "location", title: "Location" },
-        { id: "area", title: "Area" },
-        { id: "category", title: "Category" },
-      ],
-    });
-    return csvWriter.stringifyRecords(orderDetails);
-  } catch (error) {
-    return "";
-  }
 }
